b1f9cd538f49ee0f06c9a9e3e041998b048fe989
Vincent <soothion@sina.com>
2016-04-20 09:37:12 +0800

    fix

--- protected/controllers/BlogController.php
+++ protected/controllers/BlogController.php
@@ -13,6 +13,8 @@ class BlogController extends Controller{
             if($_SESSION['openid']!=$user->openid){
                 header("Content-type: text/html; charset=utf-8");
                 echo '<h2>此帐号已绑定其他微信帐号</h2>';
+                $login = $this->createUrl('/login/index');
+                echo "<a href='{$login}'>切换帐号</a>";
                 die;
             }
         }
